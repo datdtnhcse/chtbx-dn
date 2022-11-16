b2025da204d1e797e21fab90c3dea9e2db4fb133
Nguyễn Phúc Bình Khang <91519610+KhangBenTree@users.noreply.github.com>
2022-11-16 11:47:21 +0700

    Revert "fix unsupport sub-protocol in Edge"

--- webapp/main.ts
+++ webapp/main.ts
@@ -1,6 +1,6 @@
 import * as esbuild from "https://deno.land/x/esbuild@v0.15.13/mod.js";
 import * as importMap from "https://esm.sh/esbuild-plugin-import-map@2.1.0";
-import { WEBAPP_PORT, SUBWEBSOCKET_PORT, WEBSOCKET_PORT } from "../env.ts";
+import { WEBAPP_PORT, WEBSOCKET_PORT } from "../env.ts";
 
 await importMap.load(["./import_map.json"]);
 await esbuild.serve({
@@ -17,6 +17,5 @@ await esbuild.serve({
 	plugins: [importMap.plugin()],
 	define: {
 		"window.WEBSOCKET_PORT": `${WEBSOCKET_PORT}`,
-		"window.SUBWEBSOCKET_PORT": `${SUBWEBSOCKET_PORT}`,
 	},
 });
