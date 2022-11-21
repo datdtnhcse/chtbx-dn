1ef6a6c762d3a22f4542c9e82cf5a63179bef9e9
datdtnhcse <dat.dtnhcse@gmail.com>
2022-11-21 09:46:12 +0700

    update db.ts

--- db.ts
+++ db.ts
@@ -4,13 +4,23 @@ const db = new DB();
 
 db.execute(`
 CREATE TABLE IF NOT EXISTS accounts (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	username TEXT NOT NUll UNIQUE,
-	password TEXT NOT NULL,
-	ip TEXT
+	id 			INTEGER 	PRIMARY KEY AUTOINCREMENT,
+	username 	TEXT 		NOT NUll UNIQUE,
+	password 	TEXT 		NOT NULL,
+	ip 			TEXT
 );
 INSERT INTO accounts(username, password) VALUES ('khang', '123456');
+INSERT INTO accounts(username, password) VALUES ('dat', '123456');
+
+CREATE TABLE IF NOT EXISTS friends (
+	id 			INTEGER 									NOT NULL,
+	friendId 	INTEGER 									NOT NULL,
+	state 		TEXT CHECK(state IN ('sent', 'friended'))   NOT NULL, 
+	PRIMARY KEY (id, friendId)
+);
 `);
+
+// accounts Query 
 
 export const findAccount = db.prepareQuery<
 	never,
@@ -35,4 +45,24 @@ export const setIP = db.prepareQuery<
 UPDATE accounts
 SET ip = :ip
 WHERE id = :id
+;`);
+
+// friends Query
+
+export const sendRequest = db.prepareQuery<
+	never,
+	never,
+	{ id: number; friendId: number }
+>(
+	"INSERT INTO friends(id, friendId, state) VALUES (:id, :friendId, 'sent');",
+);
+
+export const acceptRequest = db.prepareQuery<
+	never,
+	never,
+	{ id: number; friendId: number }
+>(`
+UPDATE friends
+SET state = 'friended'
+WHERE friendId = :id AND id = :friendId 
 ;`);
