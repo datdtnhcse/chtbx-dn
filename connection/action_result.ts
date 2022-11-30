1add2277d72d33136bbd2f09914be355b9a6112c
Khang Nguyen Duy <khang.nguyenduycse@hcmut.edu.vn>
2022-12-01 00:20:17 +0700

    sync helpers

--- connection/action_result.ts
+++ connection/action_result.ts
@@ -1,8 +1,11 @@
 import {
 	actionKey,
 	ActionMap,
+	ActionType,
+	GUIState,
 	resultKey,
 	ResultMap,
+	ResultType,
 } from "../protocol/action_result.ts";
 import { WebSocketConnection } from "./mod.ts";
 
@@ -11,11 +14,17 @@ export class WebSocketActionResult
 	constructor(socket: WebSocket, label: string) {
 		super(socket, actionKey, resultKey, label);
 	}
+	sync() {
+		this.send({ type: ActionType.SYNC });
+	}
 }
 
 export class WebSocketResultAction
 	extends WebSocketConnection<ResultMap, ActionMap> {
 	constructor(socket: WebSocket, label: string) {
 		super(socket, resultKey, actionKey, label);
+	}
+	sync(state: GUIState) {
+		this.send({ type: ResultType.SYNC, state });
 	}
 }
